6a633ed720cf3efb12d758d7adf749e8e98abd48
chririva <c.riva9@studenti.unibg.it>
2018-04-25 11:36:00 +0200

    update3

--- Server.py
+++ Server.py
@@ -26,6 +26,7 @@ while SERVER_STATUS == 0:
                 if data_arr[3] == 1:
                     print("STARTING TIC TAC TOE")
                     game = TicTacToe()
+                    game.set_name_player1(data_arr[2])
                     if data_arr[1] == 1:
                         SERVER_STATUS = 1
                     else:
@@ -52,5 +53,29 @@ while SERVER_STATUS == 0:
                 if data_arr[0] == 2:
                     #request battlefield
                     conn.send(pickle.dumps([2, game.get_battlefield()]))
-
-    conn.close()
+                elif data_arr[0] == 3:
+                    #put choice
+                    game.put_choice(data_arr[1],data_arr[2])
+                    conn.send(pickle.dumps([3,1]))
+                elif data_arr[0] == 4:
+                    #status of the match
+                    status = game.status_of_the_match()
+                    if status != 0:
+                        print("game is ended")
+                    conn.send(pickle.dumps([4,status]))
+                elif data_arr[0] == 5:
+                    #get next
+                    conn.send(pickle.dumps([5, game.get_next()]))
+                elif data_arr[0] == 6:
+                    #is free
+                    conn.send(pickle.dumps([6, game.is_free(data_arr[1])]))
+                elif data_arr[0] == 7:
+                    #computer turn
+                    conn.send(pickle.dumps([7, game.computer_turn()]))
+                elif data_arr[0] == -1:
+                    #CLOSE CONNECTION
+                    print("CONNECTION CLOSED")
+                    SERVER_STATUS = 0
+                    conn.send(pickle.dumps([-1]))
+                    conn.close()
+                    break
--- TicTacToeClientConnection.py
+++ TicTacToeClientConnection.py
@@ -18,13 +18,17 @@ class TicTacToeClientConnection:
                     return 1
                 return 0
     def close_connection(self):
+        self.s.send(pickle.dumps([-1]))
+        data_received = pickle.loads(self.s.recv(4096))
         self.s.close()
+
     def initial_settings(self,number_of_player,my_name,which_game):
         data_to_send = ([1,number_of_player,my_name,which_game])
         arr = pickle.dumps(data_to_send)
         self.s.send(arr)
         #data = self.s.recv(4096)
         #data_received = pickle.loads(data)
+
     def battlefield_request(self):
         data_to_send = ([2])
         arr = pickle.dumps(data_to_send)
@@ -32,3 +36,27 @@ class TicTacToeClientConnection:
         data = self.s.recv(4096)
         data_received = pickle.loads(data)
         return data_received[1]
+
+    def put_choice(self,player,location):
+        self.s.send(pickle.dumps([3,player,location]))
+        data = self.s.recv(4096)
+        data_received = pickle.loads(data)
+
+    def status_of_the_match(self):
+        self.s.send(pickle.dumps([4]))
+        data_received = pickle.loads(self.s.recv(4096))
+        return data_received[1]
+
+    def get_next(self):
+        self.s.send(pickle.dumps([5]))
+        data_received = pickle.loads(self.s.recv(4096))
+        return data_received[1]
+
+    def is_free(self,choice):
+        self.s.send(pickle.dumps([6,choice]))
+        data_received = pickle.loads(self.s.recv(4096))
+        return data_received[1]
+
+    def computer_turn(self):
+        self.s.send(pickle.dumps([7]))
+        data_received = pickle.loads(self.s.recv(4096))
